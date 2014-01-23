7461b4e1bf53cc91f918df115d750d38bb1dccc2
Pietro Bertera <pietro@bertera.it>
2014-01-23 18:30:06 +0100

    Added TCP support for iptables-SIPu32.py

--- iptables-SIPu32.py
+++ iptables-SIPu32.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 """
-This script produces a GNU/Linux iptables u32 rule matching against SIP requests.
+This script produce a GNU/Linux iptables u32 rule matching against SIP requests.
 
 The match is against the first line of the SIP message
 
 Usage:
 
-./iptables-SIPu32.py REGISTER sip:100@pbx.domain.local
+./u32SIP.py REGISTER sip:100@pbx.domain.local
 
-iptables -I OUTPUT 1 -p udp  \! -f -m u32 --u32 $(./iptables-SIPu32.py "INVITE sip:3381234567") -j LOG --log-prefix "INVITE to my cell"
+
 """
 
 
@@ -25,24 +25,37 @@ def flatten(array):
         result += char
     return result
 
-if len(sys.argv) < 2:
-    print "Usage: %s [-d] 'text'" % sys.argv[0]
+if len(sys.argv) < 3:
+    print "Usage: %s [-d] udp|tcp 'text'" % sys.argv[0]
     print
     print "Example: "
     print "%s 'INVITE sip:ciccio.pasticcio@example.com:5060'\t\t to match all INVITE to sip:ciccio.pasticcio@example.com:5060" % sys.argv[0] 
-    print "\nThe -d switch enables the debugging (not suitable for embedding in iptables commands)"
-    print "\n"
-    print "This script works only with SIP over UDP."
+    print "the -d switch enables the debugging (not suitable for embedding in iptables commands)"
     sys.exit(-1)
    
-   
-search = sys.argv[1]
+proto = sys.argv[1]
+search = sys.argv[2]
 debug = False
+
+tcp_base = "0>>22&0x3C@12>>26&0x3C@"
+tcp_offset = 0
+udp_base = "0>>22&0x3C@"
+udp_offset = 8
 
 if sys.argv[1] == "-d":
     debug = True
-    search = sys.argv[2]
+    proto = sys.argv[2]
+    search = sys.argv[3]
 
+if proto == "udp":
+    base = udp_base
+    offset = udp_offset
+elif proto == "tcp":
+    base = tcp_base
+    offset = tcp_offset
+else:
+    print "Error: proto must be udp or tcp"
+    sys.exit (-1)
 
 step = 0
 group = 0
@@ -54,14 +67,14 @@ for char in search:
     hexstring.append("%02x" % ord(char))
     if (step % 4) == 0:
         if debug:
-            print "POSITION: 0>>22&0x3C@%i VALUE: %s (%s)" % ( (8+(group*4)), flatten(hexstring), "".join(map(hex2char, hexstring)))
+            print "POSITION: %s%i VALUE: %s (%s)" % ( base, (offset + (group*4)), flatten(hexstring), "".join(map(hex2char, hexstring)))
         else:
-            sys.stdout.write("0>>22&0x3C@%i=%s" % ( (8+(group*4)), flatten(hexstring)))
+            sys.stdout.write("%s%i=%s" % ( base, (offset + (group*4)), flatten(hexstring)))
         hexstring = []
         group += 1
         if not debug:
             if step != len(search):
-                sys.stdout.write("&&")
+                sys.stdout.write(" && ")
             else:
                 sys.stdout.write("")
 if len(hexstring):
@@ -72,6 +85,6 @@ if len(hexstring):
 
     if debug:
         print "BITMASK: 0x%s" % "".join(maskstring)
-        print "POSITION: 0>>22&0x3C@%i&0x%s VALUE: %s (%s)" % ( (8+(group*4)), "".join(maskstring), flatten(hexstring), "".join(map(hex2char, hexstring)))
+        print "POSITION: %s%&0x%s VALUE: %s (%s)" % ( base, (offset + (group*4)), "".join(maskstring), flatten(hexstring), "".join(map(hex2char, hexstring)))
     else:
-        sys.stdout.write("0>>22&0x3C@%i&0x%s=%s" % ( (8+(group*4)), "".join(maskstring), flatten(hexstring)))
+        sys.stdout.write("%s%i&0x%s=%s" % (base, (offset + (group*4)), "".join(maskstring), flatten(hexstring)))
