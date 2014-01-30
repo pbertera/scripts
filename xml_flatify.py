6864d7b3c51fe920452127521d2726fabc58c9f1
Pietro Bertera <pietro@bertera.it>
2014-01-30 15:54:33 +0100

    * new flatify

--- xml_flatify.py
+++ xml_flatify.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#
+# 
 
 import sys
 import StringIO
@@ -10,95 +12,98 @@ from xml.dom.minidom import Node
 first = True
 out = StringIO.StringIO()
 
-def gtn(elm):
-    """ get tag name"""
-    if elm.nodeType == Node.TEXT_NODE:
-        return "aaaaaaaa"
-    else:
-        return elm.tagName.lower()
+def _flat(elm, options, base="", depth=0):
+	global first, out
+	
+	tname = elm.tagName.lower()
+	base = res = base+"/"+tname
+	
 
-def print_tag(elm, options, base="", depth=0):
-    global first, out
+	# if this tag should be exluded skip the whole subtree
+	if len(options.exclude_tag) > 0:
+		if tname in [t.lower() for t in options.exclude_tag]:
+			return ""
+	
+	childnodes = [] 
+	for e in elm.childNodes:
+		if e.nodeType == Node.ELEMENT_NODE:
+			r = _flat(e, options=options, base=base, depth=depth)
+			if r!="":
+				childnodes.append(r)
 
-    if elm.nodeType == Node.ELEMENT_NODE:
-        # Element is a node, print name and addributes
-        tname = elm.tagName.lower()
-        # if this tag should be exluded skip the whole subtree
-        if len(options.exclude_tag) > 0:
-            if tname in [t.lower() for t in options.exclude_tag]:
-                return
-        if not first:
-            out.write("\n\r")
-        
-        # attributes are ordered by name 
-        attrs = elm.attributes
-        sattrs = "&".join( 
-                sorted( 
-				[ k.lower()+'="'+attrs[k].value.lower().strip()+'"' for k in attrs.keys() if attrs[k].value.lower().strip() != "" ]
-			 )
+	for l in sorted(childnodes):
+		out.write(l)
+
+	# attributes are ordered by name 
+	attrs = elm.attributes
+	sattrs = "&".join( 
+		sorted( 
+			[ k.lower()+'="'+attrs[k].value.lower().strip()+'"' for k in attrs.keys() if attrs[k].value.lower().strip() != "" ]
 		)
-        r = base+"/"+tname
-        if sattrs!="":
-           r+="?"+sattrs
-        out.write(r)
-        base = base+"/"+tname
-    elif elm.nodeType == Node.TEXT_NODE:
-        # text: print it
-        text =  (elm.nodeValue or "").lower().strip()
-        if text != "":
-            out.write(" = %s" % text)
+	)
+	if sattrs!="":
+	   res+="?"+sattrs
 
-    first = False
-    
-    # child nodes ordered by name
-    childnodes = dict( [ (gtn(e), e)  for e in elm.childNodes] )
+	has_text=False
+	for e in elm.childNodes:
+		if e.nodeType == Node.TEXT_NODE:
+			text =  (e.nodeValue or "").lower().strip()
+			if text != "":
+				if not has_text:
+					res=res+" ="
+				res += " %s" % text
+				has_text=True
 
-    for key in sorted(childnodes.iterkeys()):
-        print_tag(childnodes[key], options=options, base=base, depth=depth)
+	first = False
+	return res+"\n"
+
+def flat(elm, options):
+	global first, out
+	out.write(_flat(elm,options))
 
 def main():
-    global out
-    usage = "usage: %prog [options]\n %procs create a flat and sorted output of an XML file and calucate a checksum."
-    parser = OptionParser(usage)
-    parser.add_option("-f", "--file", dest="filename", default=sys.stdin,
-            help="read from FILE (default read from stdin)", metavar="FILE")
-    parser.add_option("-d", "--dump",
-            action="store_true", dest="dump", default=False,
-            help="dump the flatified file")
-    parser.add_option("-e", "--exclude-tag", 
-            action="append", dest="exclude_tag", default=[],
-            help="exclude this tag only (default none)")
-    parser.add_option("-a", "--algorithm", dest="algorithm", default="md5",
-            help="algorithm to compute the checksum (available: md5, sha1) (default: md5)")
+	global out
+	usage = "usage: %prog [options]\n %procs create a flat and sorted output of an XML file and calucate a checksum."
+	parser = OptionParser(usage)
+	parser.add_option("-f", "--file", dest="filename", default=sys.stdin,
+			help="read from FILE (default read from stdin)", metavar="FILE")
+	parser.add_option("-d", "--dump",
+			action="store_true", dest="dump", default=False,
+			help="dump the flatified file")
+	parser.add_option("-e", "--exclude-tag", 
+			action="append", dest="exclude_tag", default=[],
+			help="exclude this tag only (default none)")
+	parser.add_option("-a", "--algorithm", dest="algorithm", default="md5",
+			help="algorithm to compute the checksum (available: md5, sha1) (default: md5)")
 
-    (options, args) = parser.parse_args()
-    if options.filename != sys.stdin:
-        fname = options.filename
-        try:
-            options.filename = open(fname)
-        except Exception, e:
-            print "Error: cannot open %s: %s" % (fname, e)
+	(options, args) = parser.parse_args()
+	if options.filename != sys.stdin:
+		fname = options.filename
+		try:
+			options.filename = open(fname)
+		except Exception, e:
+			print "Error: cannot open %s: %s" % (fname, e)
 
-    doc = minidom.parse(options.filename)
-    root = doc.documentElement
-    print_tag(root, options=options)
+	doc = minidom.parse(options.filename)
+	root = doc.documentElement
+	flat(root, options=options)
 
-    if options.filename != sys.stdin:
-        try:
-            options.filename.close()
-        except Exception, e:
-            print "Error: cannot close %s: %s" % (fname, e)
+	if options.filename != sys.stdin:
+		try:
+			options.filename.close()
+		except Exception, e:
+			print "Error: cannot close %s: %s" % (fname, e)
 
-    out.write("\n\r")
-    if options.dump:
-        print out.getvalue()
-    
-    if options.algorithm == "md5":
-        print "md5sum: " + hashlib.md5(out.getvalue()).hexdigest()
-    elif options.algorithm == "sha1":
-        print "sha1sum: " + hashlib.sha1(out.getvalue()).hexdigest()
-    else:
-        print "The algorithm isn't supported"
+	#out.write("\n\r")
+	if options.dump:
+		print out.getvalue()
+	
+	if options.algorithm == "md5":
+		print "md5sum: " + hashlib.md5(out.getvalue()).hexdigest()
+	elif options.algorithm == "sha1":
+		print "sha1sum: " + hashlib.sha1(out.getvalue()).hexdigest()
+	else:
+		print "The algorithm isn't supported"
 
 if __name__ == '__main__':
-    main()
+	main()
